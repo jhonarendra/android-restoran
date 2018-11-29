8d06cf4aeaa70da936bff3e3e4d7cbe381b81a88
jhonarendra <jhonarendra@gmail.com>
2018-11-29 00:29:18 -0800

    fcm

---
+++ app/src/main/java/com/jhonarendra/restoran/customer/MyFirebaseMessagingService.java
@@ -0,0 +1,10 @@
+package com.jhonarendra.restoran.customer;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Jhonarendra on 11/29/2018.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
